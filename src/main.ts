b3620759543be05f7b8afb9c2daf35e854e1e452
Mogeni <evansmogeni254@gmail.com>
2026-09-12 00:47:54 +0300

    Update start script in package.json to use node and modify app.listen to bind to all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, swaggerDocument);
 
   const port = configService.get<number>('port') ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 
 void bootstrap();
